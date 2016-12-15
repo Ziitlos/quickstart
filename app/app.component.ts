86aacacfa0748f95cd6ff643806011e16e710778
Hannes Ehrensperger <hannes.ehrensperger@helsana.ch>
2016-12-15 12:24:18 +0100

    Hero Objekt in app.component platziert. Überflüssiges File gelöscht, da die Class direkt im app.component ist. FormsModule und input field für two way binding eingefügt.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,32 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <h1>{{title}}</h1>
-  <h2>My favorite hero is: {{myHero.name}}</h2>
-  <p>Heroes:</p>
-  <ul>
-    <li *ngFor="let hero of heroes">
-      {{ hero.name }}
-      </li>
-  </ul>
-  <p *ngIf="heroes.length > 3">There are many heroes!</p>
-`
+  template: `<h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div>
+    <label>id: </label>{{hero.id}}
+  </div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>`
 })
+
+
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = [
-    new Hero(1, 'Windstorm'),
-    new Hero(13, 'Bombasto'),
-    new Hero(15, 'Magneta'),
-    new Hero(20, 'Tornado')
-  ];
-  myHero = this.heroes[0];
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
 }
+
+
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
