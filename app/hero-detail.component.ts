f5fe0f9e10db8266eb59033bdb81c49d00b27780
Hannes Ehrensperger <hannes.ehrensperger@helsana.ch>
2016-12-19 16:25:23 +0100

    Routing eingebaut

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -2,29 +2,42 @@
  * Created by hkfq4 on 15.12.2016.
  */
 
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import { Location } from '@angular/common';
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
+import {ActivatedRoute, Params} from '@angular/router';
+
+import 'rxjs/add/operator/switchMap';
+
 
 @Component({
-
+  moduleId: module.id,
   selector: 'my-hero-detail',
-  template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  </div>
-  `
-
+  templateUrl: 'hero-detail.component.html',
+  styleUrls: [ 'hero-detail.component.css' ]
 })
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
 
   @Input()
   hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 
 }
 
